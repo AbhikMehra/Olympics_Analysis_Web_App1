e0d11b0134373590da3089585438f8245c4d85a6
AbhikMehra <mehraabhik23@gmail.com>
2025-10-25 23:12:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.figure_factory as ff
+import scipy  # Required by plotly's distplot
 
 # ------------------ DATA LOADING ------------------
 df = pd.read_csv('athlete_events.csv')
